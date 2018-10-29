b080cae6a20f0a721999c1adec4856cebd81a85f
Agskvortsov <agskvortsov@gmail.com>
2018-10-29 19:52:30 +0200

    Home_work

--- news_aggregator/models.py
+++ news_aggregator/models.py
@@ -1,3 +1,18 @@
+from datetime import timezone
+
 from django.db import models
 
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField()
+    resource = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.title
+
