90cb65877c38a169b0e1ccfd83ea0c2269de22a4
Djet78 <vacheslavuslistyj@gmail.com>
2018-10-29 23:39:09 +0200

    - I added news_aggregator/models.py.  - Registered them in news_aggregator/admin.py.  - Added our app in INSTALLED_APPS in news_aggregator_api/settings.py

--- news_aggregator/models.py
+++ news_aggregator/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=30, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Resource(models.Model):
+
+    name = models.CharField(max_length=50, unique=True)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{}, {}".format(self.name, self.location)
+
+
+class News(models.Model):
+
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    tag = models.ManyToManyField(Tag)
+    resource = models.ManyToManyField(Resource)
+
+    class Meta:
+        verbose_name_plural = 'news'
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+    password = models.CharField(max_length=40)
+
+    def __str__(self):
+        return "{} {}".format(self.first_name,  self.last_name)
