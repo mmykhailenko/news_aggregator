f127e3c50b7c7c9eee8a937161567742b2ddc2f0
yozzik <mailyozzik@gmail.com>
2018-11-01 00:03:48 +0200

    Created views, urls and templates for News (list, detail), Tag (list), Resource (list), Category (list)

---
+++ news_aggregator/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.views import generic
+from news_aggregator import views
+
+app_name = 'news_aggregator'
+urlpatterns = [
+    path('news/', views.NewsListView.as_view(), name='news_list'),
+    path('news/<int:pk>', views.NewsDetailView.as_view(), name='news_single'),
+    path('tags/', views.TagListView.as_view(), name='tag_list'),
+    path('resource/', views.ResourceListView.as_view(), name='resource_list'),
+]
