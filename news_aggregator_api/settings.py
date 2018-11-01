2d617b77392f99652538a38f3e77eaedd9a7e6f4
Maksim Mikhailenko <maksim.mikhailenko@sigmaukraine.com>
2018-11-01 19:47:00 +0200

    Fix settings and docker-compose.yaml

--- news_aggregator_api/settings.py
+++ news_aggregator_api/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'news_aggregator_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'news_db',
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST': 'dbPostgres',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
