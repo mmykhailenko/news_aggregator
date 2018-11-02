f6609db3b9dca0e9cec1555998d83b2c2161cb79
Djet78 <vacheslavuslistyj@gmail.com>
2018-11-02 17:37:13 +0200

    - Templates added:      base.html      news_detail.html  - Redundant data removed from views  - Now templates inherits from base.html

--- news_aggregator/urls.py
+++ news_aggregator/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = 'news_aggregator'
 
 urlpatterns = [
-    path('', views.NewsView.as_view()),
-    path('user/', views.UserView.as_view()),
-    path('tag/', views.TagView.as_view()),
-    path('category/', views.CategoryView.as_view()),
-    path('resource/', views.ResourceView.as_view()),
+    path('', views.NewsView.as_view(), name='news_list.html'),
+    path('<int:pk>/', views.NewsDetailView.as_view(), name='news_detail.html'),
+    path('user/', views.UserView.as_view(), name='user_list.html'),
+    path('tag/', views.TagView.as_view(), name='tag_list.html'),
+    path('category/', views.CategoryView.as_view(), name='category_list.html'),
+    path('resource/', views.ResourceView.as_view(), name='resource_list.html'),
 ]
--- news_aggregator/views.py
+++ news_aggregator/views.py
@@ -5,28 +5,29 @@ from .models import User, News, Tag, Category, Resource
 class NewsView(generic.ListView):
 
     model = News
-    template_name = 'news_list'
+
+
+class NewsDetailView(generic.DetailView):
+
+    model = News
+    context_object_name = 'news'
 
 
 class UserView(generic.ListView):
 
     model = User
-    template_name = 'user_list'
 
 
 class TagView(generic.ListView):
 
     model = Tag
-    template_name = 'tag_list'
 
 
 class CategoryView(generic.ListView):
 
     model = Category
-    template_name = 'category_list'
 
 
 class ResourceView(generic.ListView):
 
     model = Resource
-    template_name = 'resource_list'
