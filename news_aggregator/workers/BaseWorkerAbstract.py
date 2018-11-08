43257565f9e20b7fdccfc4b898e455bdb494ed7d
yozzik <mailyozzik@gmail.com>
2018-11-08 23:58:51 +0200

    non-repeating workers

---
+++ news_aggregator/workers/BaseWorkerAbstract.py
@@ -0,0 +1,10 @@
+class BaseWorkerAbstract:
+
+    def _auth(self):
+        raise NotImplementedError
+
+    def _url_construct(self):
+        raise NotImplementedError
+
+    def _validate_data(self):
+        raise NotImplementedError
