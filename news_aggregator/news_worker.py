4d2cf8f7d13b034b2cb5a56994b8b049a46e165b
yozzik <mailyozzik@gmail.com>
2018-11-08 20:40:56 +0200

    update

--- news_aggregator/news_worker.py
+++ news_aggregator/news_worker.py
@@ -20,38 +20,42 @@ class BaseWorker:
     def _validate_data(self):
         raise NotImplementedError
 
-class NewsAPIWorker(BaseWorker):
+class NewsApiWorker(BaseWorker):
 
-    BASE_URL = "https://newsapi.org/"
-    API_VERSION = "v2/"
-    API_TYPE = "everything"
+    BASE_URL = "https://newsapi.org"
+    API_VERSION = "v2"
+    API_TYPE = "top-headers"
     API_KEY = "488c6cc7d5b04c3ea87b8d672152eeaf"
 
-    def __init__(self, base_url=NewsAPIWorker.BASE_URL, api_version=NewsAPIWorker.API_VERSION, api_type=NewsAPIWorker.API_TYPE, api_key=NewsAPIWorker.API_KEY, country="USA", category, lang="en"):
-        self.base_url = base_url
-        self.api_version = api_version
-        self.api_type = api_type
+    def __init__(self, api_type, api_key, query, country, category, language, time_step):
+        self.base_url = self.BASE_URL
+        self.api_version = self.API_VERSION
+        self.api_type = api_type or "top-headers"
         self.api_key = api_key
-        self.country = country
+        self.country = country or "ua"
+        self.query = query
         self.category, _ = Category.objects.get_or_create(name=category)
-        self.language = lang
-        self.time_step = 10
+        self.language = language or "uk"
+        self.time_step = time_step or 10
         self.url = self._url_create()
         self.data = dict()
 
 
     def _url_create(self):
-        return f"{self.base_url}{self.api_version}{self.api_type}?apiKey={self.api_key}"
-        postfix_url = f'pageSize=100&q={self.category}&language={self.language}'
-        # url = urljoin(self.base_url, self.API_VERSION, self.API_TYPE, postfix_url)
-        url = f"https://newsapi.org/v2/everything?apiKey=488c6cc7d5b04c3ea87b8d672152eeaf&{postfix_url}"
+        base_url = f"{self.base_url}/{self.api_version}/{self.api_type}?apiKey={self.api_key}"
+        postfix_url = f"&q={self.query}"
 
-        return url
+        if self.api_type == "everything":
+            postfix_url += f"language={self.language}"
+        elif self.api_type == "top-headers":
+            postfix_url += f"country={self.country}"
+
+        return f"{base_url}{postfix_url}"
 
     def get_news(self):
 
         with requests.Session() as session:
-            response = session.get(self.url_base).json()
+            response = session.get(self.url).json()
             self._serialize_news(response['articles'])
 
     def _serialize_news(self, raw_news):
@@ -67,5 +71,11 @@ class NewsAPIWorker(BaseWorker):
                                                    res = n_source)[0]
 
 
+
+# class WorkerResponse:
+#     def __init__(self):
+#         self.title
+#         self.url
+#         self.content
 
 # TODO: make scheduled workers
