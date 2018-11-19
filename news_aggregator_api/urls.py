bd036c3c9bb6699ae687ccae06c8af0dc064898d
Djet78 <vacheslavuslistyj@gmail.com>
2018-11-19 21:19:08 +0200

    Created user auth

--- news_aggregator_api/urls.py
+++ news_aggregator_api/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from news_aggregator.views import Register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('news_aggregator.urls')),
+    path('register/', Register.as_view(), name='register'),
 ]
