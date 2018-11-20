7cfa8e2cc3d86783e5015f38d89ad20de56de860
Djet78 <vacheslavuslistyj@gmail.com>
2018-11-20 10:36:05 +0200

    - worker launch from 'news_aggregator/apps.py' removed to  'news_aggregator/workers/worker_run.py'

--- news_aggregator/apps.py
+++ news_aggregator/apps.py
@@ -19,16 +19,6 @@ class NewsAggregatorConfig(AppConfig):
             if not self.is_worker_running:
                 self.is_worker_running = True
 
-                from time import sleep
-                from .workers.collectors.news_api_collector import NewsAPICollector
-                from .workers.serializers.news_api_serializer import NewsAPISerializer
+                from .workers.worker_run import news_api_worker_run
 
-                news_api_collector = NewsAPICollector('news_api_collector')
-                news_api_serializer = NewsAPISerializer('news_api_serializer')
-
-                while True:
-                    news_api_collector.collect()
-                    news_api_serializer.serialize(news_api_collector.news_storage,
-                                                  country=news_api_collector.QUERY_PARAMS['country'],
-                                                  lang=news_api_collector.QUERY_PARAMS['language'])
-                    sleep(15)
+                news_api_worker_run()
---
+++ news_aggregator/workers/worker_run.py
@@ -0,0 +1,15 @@
+from time import sleep
+from .collectors.news_api_collector import NewsAPICollector
+from .serializers.news_api_serializer import NewsAPISerializer
+
+
+def news_api_worker_run():
+    news_api_collector = NewsAPICollector()
+    news_api_serializer = NewsAPISerializer()
+
+    while True:
+        news_api_collector.collect()
+        news_api_serializer.serialize(news_api_collector.news_storage,
+                                      country=news_api_collector.QUERY_PARAMS['country'],
+                                      lang=news_api_collector.QUERY_PARAMS['language'])
+        sleep(15)
