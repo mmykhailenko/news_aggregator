065a40b3a7f65b1eb1a0d24ac2584567c01dc339
Djet78 <vacheslavuslistyj@gmail.com>
2018-11-30 16:35:28 +0200

    integration test of News API serializer and collector

---
+++ news_aggregator/tests/test_workers/test_news_api_worker.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from news_aggregator.models import News
+from news_aggregator.workers.serializers import NewsAPISerializer
+from news_aggregator.workers.collectors import NewsAPICollector
+
+
+class TestNewsAPIWorker(TestCase):
+    """
+        Binds work of collector and serializer
+    """
+
+    news_api_collector = NewsAPICollector()
+    news_api_serializer = NewsAPISerializer()
+
+    def test_is_data_collected(self):
+        self.news_api_collector.collect()
+        self.news_api_serializer.serialize(self.news_api_collector.news_storage,
+                                           country=self.news_api_collector.QUERY_PARAMS['country'],
+                                           lang=self.news_api_collector.QUERY_PARAMS['language'])
+        self.assertTrue(News.objects.all())
